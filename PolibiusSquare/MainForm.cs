c3b1d1af70c2413dc5bfdd10aef09f6770e00e44
seekin4u <wagamama47@gmail.com>
2018-03-21 15:13:26 +0200

    Renaming and funks prototypes

--- PolibiusSquare/MainForm.cs
+++ PolibiusSquare/MainForm.cs
@@ -1,13 +1,4 @@
-﻿#define DEB
-
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 using System.Diagnostics;
 
@@ -15,8 +6,26 @@ namespace PolibiusSquare
 {
     public partial class MainForm : Form
     {
-        String inputString = "";
-        String outputString = "";
+        /// <summary>
+        /// Поле, содержащее изначальный вариант входного текста.
+        /// </summary>
+        private static String inputString = "";
+
+        /// <summary>
+        /// Поле, содержащее окончательный вариант выходного текста.
+        /// </summary>
+        private static String outputString = "";
+
+        /// <summary>
+        /// Массив, содержащий в себе символы алфавита, который будет тешифроваться
+        /// </summary>
+        private static Char[,] Alphabet = {
+                            {'A', 'B', 'C', 'D', 'E'},
+                            {'F', 'G', 'H', 'I', 'K'}, //i is the same as j so we can skeep this leter
+                            {'L', 'M', 'N', 'O', 'P'},
+                            {'Q', 'R', 'S', 'T', 'U'},
+                            {'V', 'W', 'X', 'Y', 'Z'}
+                           };
 
         public MainForm()
         {
@@ -25,18 +34,26 @@ namespace PolibiusSquare
             
         }
 
+        private void pencypt(String incomingMessage)//polibius encrypt
+        {
+            Debug.WriteLine("Entered pencrypt");
+        }
+
+
         private void MainForm_Load(object sender , EventArgs e)
         {
-
+            //TODO
         }
 
         private void encrytpButton_Click(object sender , EventArgs e)
         {
-            inputString = Text;
+            Debug.WriteLine("entered encryptButton_Click");
+            inputString = inputTextBox.Text;
 
-            String bufferString = Text;
-
-
+            String bufferString = inputTextBox.Text;
+            bufferString = bufferString.Replace(" " , string.Empty);//Clearing spaces
+            bufferString = bufferString.ToUpper();
+            Debug.WriteLine("uppercased string without spaces :" + bufferString);
 
             outputString = bufferString;
         }
